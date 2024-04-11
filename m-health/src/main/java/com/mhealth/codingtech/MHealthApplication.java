a2829a211e7b4de10e442a4aa92529faf40fdd18
Mr.Khalil laafou <116034601+khalillf@users.noreply.github.com>
2024-04-11 03:05:14 +0000

    Initial backend files

---
+++ m-health/src/main/java/com/mhealth/codingtech/MHealthApplication.java
@@ -0,0 +1,13 @@
+package com.mhealth.codingtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MHealthApplication.class, args);
+	}
+
+}
---
+++ m-health/src/test/java/com/mhealth/codingtech/MHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhealth.codingtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
