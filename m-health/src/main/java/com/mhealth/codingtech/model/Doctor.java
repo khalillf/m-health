760ae5ccd6721e19e51f3ae5240117db520e8e30
Mr.Khalil laafou <116034601+khalillf@users.noreply.github.com>
2024-04-18 23:52:06 +0100

    add doctor entity

---
+++ m-health/src/main/java/com/mhealth/codingtech/model/Doctor.java
@@ -0,0 +1,49 @@
+package com.mhealth.codingtech.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table( name = "doctors")
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_doctor;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "specialty", nullable = false)
+    private String specialty;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @ManyToMany(mappedBy = "doctors")
+    private Set<Patient> patients = new HashSet<>();
+
+    @OneToMany(mappedBy = "doctor")
+    private List<Appointment> appointments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "doctor")
+    private List<Prescription> prescriptions = new ArrayList<>();
+
+
+
+}
