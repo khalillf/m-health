a45b4b03bb52db9c9571a51aa25b051ed286ec09
Mr.Khalil laafou <116034601+khalillf@users.noreply.github.com>
2024-04-19 00:45:29 +0100

    add Admin entity

---
+++ m-health/src/main/java/com/mhealth/codingtech/model/Admin.java
@@ -0,0 +1,28 @@
+package com.mhealth.codingtech.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "admins")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "role")
+    private String role;
+
+}
