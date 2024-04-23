b8474a512d67252ac1bb67c4431be79d4e9c7293
Mr.Khalil laafou <116034601+khalillf@users.noreply.github.com>
2024-04-23 14:25:58 +0100

    add Medicine Service

---
+++ m-health/src/main/java/com/mhealth/codingtech/service/MedicineService.java
@@ -0,0 +1,34 @@
+package com.mhealth.codingtech.service;
+
+import com.mhealth.codingtech.model.Medicine;
+import com.mhealth.codingtech.repository.MedicineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MedicineService {
+
+    @Autowired
+    private MedicineRepository medicineRepository;
+
+    public List<Medicine> getAllMedicines() {
+        return medicineRepository.findAll();
+    }
+
+    public Optional<Medicine> getMedicineById(Long id) {
+        return medicineRepository.findById(id);
+    }
+
+    public Medicine saveMedicine(Medicine medicine) {
+        return medicineRepository.save(medicine);
+    }
+
+    public void deleteMedicine(Long id) {
+        medicineRepository.deleteById(id);
+    }
+
+    // Add more service methods as needed
+}
